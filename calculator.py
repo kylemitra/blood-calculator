0fac98cf6efa73d9baa877cc6bd4e7cc55eb2224
Kyle Mitra <kjmitra@yahoo.com>
2020-08-28 15:35:39 -0400

    Added input function

--- calculator.py
+++ calculator.py
@@ -2,9 +2,36 @@ def interface():
     print("My Program")
     while True:
         print("Options:")
+        print("1 - HDL")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice == '9':
             return
+        elif choice == '1':
+            HDL_driver()
+
+def HDL_driver():
+    # Create get input function
+    HDLInput = input()
+
+    # Check if HDL is normal
+    calculatedHDL = HDLChecker(HDLInput)
+
+    # Output
+
+def input():
+    myInput = input('Enter your HDL: ')
+    return myInput
+
+def HDLChecker(input):
+    if input >= 60:
+        output = "Normal"
+    elif input < 40:
+        output = "Low"
+    else:
+        output = "Borderline Low"
+    return output
+
+
 
 interface()
